6d42fb5c3f26f11a2ed3abef8993e025b6afb219
Reid Mello <30907815+rjmello@users.noreply.github.com>
2023-12-28 15:50:54 -0500

    Create routes to create, update, and view a user

--- app/api/v1/routes/users.py
+++ app/api/v1/routes/users.py
@@ -1,16 +1,58 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import deps, models, schemas
 
 router = APIRouter()
 
-responses = {404: {"model": schemas.ErrorResponse}}
+
+@router.post(
+    "/",
+    response_model=schemas.User,
+    responses={status.HTTP_400_BAD_REQUEST: {"model": schemas.ErrorResponse}},
+)
+async def create_user(
+    body: schemas.UserCreate, db: Annotated[AsyncSession, Depends(deps.get_async_db)]
+):
+    user = await models.User.get(db, username=body.username, email=body.email)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"A user with the same username or email already exists.",
+        )
+
+    user = await models.User.create(db, body)
+    return user
 
 
-@router.get("/{username}", response_model=schemas.User, responses=responses)
-async def get_user(username: str, db: Annotated[AsyncSession, Depends(deps.get_async_db)]):
-    user = await models.User.get_or_404(db, username=username)
+@router.put(
+    "/me",
+    response_model=schemas.User,
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"model": schemas.ErrorResponse},
+        status.HTTP_401_UNAUTHORIZED: {"model": schemas.ErrorResponse},
+        status.HTTP_404_NOT_FOUND: {"model": schemas.ErrorResponse},
+    },
+)
+async def update_user_me(
+    body: schemas.UserUpdate,
+    user: Annotated[models.User, Depends(deps.get_active_user)],
+    db: Annotated[AsyncSession, Depends(deps.get_async_db)],
+):
+    await user.update(db, body)
+    return user
+
+
+@router.get(
+    "/me",
+    response_model=schemas.User,
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"model": schemas.ErrorResponse},
+        status.HTTP_401_UNAUTHORIZED: {"model": schemas.ErrorResponse},
+        status.HTTP_404_NOT_FOUND: {"model": schemas.ErrorResponse},
+    },
+)
+async def get_user_me(user: Annotated[models.User, Depends(deps.get_active_user)]):
     return user
